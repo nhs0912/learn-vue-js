78096bbfa8e7e7bba32fbe5e8afafc014f361f88
HeeSeok Noh <nhs0912@nate.com>
2022-04-25 00:09:29 +0900

    feat : login form 구성

---
+++ vue-form/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password">
+    </div>
+    <button>Login</button>
+  </form>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
